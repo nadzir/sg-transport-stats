1685807b4a69cb3fbd2953bd117015549fa9837d
Nadzir Azmi <nadzir_azmi@tech.gov.sg>
2018-11-09 01:57:47 +0800

    Change map. and add polyline

--- src/components/MainMap/MainMap.tsx
+++ src/components/MainMap/MainMap.tsx
@@ -1,32 +1,53 @@
-import GoogleMapReact, { Coords } from "google-map-react";
+// import GoogleMapReact, { Coords } from "google-map-react";
 import React, { Component } from "react";
+import {
+  GoogleMap,
+  Polyline,
+  withGoogleMap,
+  withScriptjs
+} from "react-google-maps";
+import DrawingManager from "react-google-maps/lib/components/drawing/DrawingManager";
+import { compose, withProps } from "recompose";
+declare var google: any;
 
 export interface IMainMapProps {
-  center: Coords;
+  // center: Coords;
   zoom: number;
 }
 
 const apiKey = process.env.GOOGLE_MAP_API_KEY || "";
 
+const MapWithPolyline = compose(
+  withProps({
+    containerElement: <div style={{ height: `1000px` }} />,
+    googleMapURL: `https://maps.googleapis.com/maps/api/js?key=${apiKey}&v=3.exp&libraries=geometry,drawing,places`,
+    loadingElement: <div style={{ height: `100%` }} />,
+    mapElement: <div style={{ height: `100%` }} />
+  }),
+  withScriptjs,
+  withGoogleMap
+)(props => (
+  <GoogleMap
+    defaultZoom={8}
+    defaultCenter={new google.maps.LatLng(-34.397, 150.644)}
+  >
+    <Polyline
+      path={[{ lat: -34.397, lng: 150.644 }, { lat: -34.397, lng: 155.644 }]}
+      options={{ strokeColor: "#ff0000" }}
+    />
+  </GoogleMap>
+));
+
 export class MainMap extends React.Component<IMainMapProps, {}> {
   public static defaultProps: Partial<IMainMapProps> = {
-    center: {
-      lat: 1.29027,
-      lng: 103.851959
-    },
-    zoom: 11
+    // center: {
+    //   lat: 1.29027,
+    //   lng: 103.851959
+    // },
+    // zoom: 11
   };
 
   public render() {
-    return (
-      // Important! Always set the container height explicitly
-      <div style={{ height: "100vh", width: "100%" }}>
-        <GoogleMapReact
-          bootstrapURLKeys={{ key: apiKey }}
-          defaultCenter={this.props.center}
-          defaultZoom={this.props.zoom}
-        />
-      </div>
-    );
+    return <MapWithPolyline />;
   }
 }
